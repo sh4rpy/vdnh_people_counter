81fca80c7e22ea5eb210243e25b78eb106746c9e
Никита Левашов <sh4rpy@ya.ru>
2020-12-23 12:21:33 +0300

    minor refactoring

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// Кнопки
 var buttonInPerson = document.getElementById("in-person");
 var buttonOutPerson = document.getElementById("out-person");
 var buttonInMinus = document.getElementById("in-minus");
 var buttonOutMinus = document.getElementById("out-minus");
 
-
+// Вывод числа вошедших/вышедших
 var resultIn = document.getElementById("result-in");
 var resultOut = document.getElementById("result-out");
 
@@ -17,6 +18,17 @@ buttonInPerson.onclick = function() {
 buttonOutPerson.onclick = function() {
   var countOut = Number(document.getElementById("result-out").value);
   countOut += 1;
+  document.getElementById("result-out").value = countOut;
+};
+// Уменьшение с помощью экранных кнопок
+buttonInMinus.onclick = function () {
+  var countIn = Number(document.getElementById("result-in").value);
+  countIn -= 1;
+  document.getElementById("result-in").value = countIn;
+};
+buttonOutMinus.onclick = function () {
+  var countOut = Number(document.getElementById("result-out").value);
+  countOut -= 1;
   document.getElementById("result-out").value = countOut;
 };
 
@@ -37,21 +49,6 @@ document.addEventListener('keydown', function(event) {
     document.getElementById("result-out").value = countOut
   }
 });
-
-
-// Уменьшение с помощью экранных кнопок
-buttonInMinus.onclick = function() {
-	var countIn = Number(document.getElementById("result-in").value);
-  countIn -= 1;
-  document.getElementById("result-in").value = countIn;
-};
-buttonOutMinus.onclick = function() {
-	var countOut = Number(document.getElementById("result-out").value);
-  countOut -= 1;
-  document.getElementById("result-out").value = countOut;
-};
-
-
 // Увеличение с помощью клавиатуры
 document.addEventListener('keydown', function(event) {
   if (event.code == 'ArrowLeft') {
