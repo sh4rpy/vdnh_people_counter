b3f7bb6b80284da2d51cb9d51c974f91507f49ef
Никита Левашов <sh4rpy@ya.ru>
2020-12-24 12:22:16 +0300

    refactoring + added a README file

--- script.js
+++ script.js
@@ -1,68 +1,23 @@
-// Кнопки
-var buttonInPerson = document.getElementById("in-person");
-var buttonOutPerson = document.getElementById("out-person");
-var buttonInMinus = document.getElementById("in-minus");
-var buttonOutMinus = document.getElementById("out-minus");
+// Увеличение/уменьшение с помощью экранный кнопок
+function increaseOrDecrease(elementId, operator) {
+  let count = Number(document.getElementById(elementId).value);
+  const result = operator == 'increase' ? count += 1 : count -= 1
+  document.getElementById(elementId).value = count;
+}
 
-
-// Вывод числа вошедших/вышедших
-var resultIn = document.getElementById("result-in");
-var resultOut = document.getElementById("result-out");
-
-
-// Увеличение с помощью экранных кнопок
-buttonInPerson.onclick = function() {
-	var countIn = Number(document.getElementById("result-in").value);
-	countIn += 1
-  document.getElementById("result-in").value = countIn;
-};
-buttonOutPerson.onclick = function() {
-  var countOut = Number(document.getElementById("result-out").value);
-  countOut += 1;
-  document.getElementById("result-out").value = countOut;
-};
-// Уменьшение с помощью экранных кнопок
-buttonInMinus.onclick = function () {
-  var countIn = Number(document.getElementById("result-in").value);
-  countIn -= 1;
-  document.getElementById("result-in").value = countIn;
-};
-buttonOutMinus.onclick = function () {
-  var countOut = Number(document.getElementById("result-out").value);
-  countOut -= 1;
-  document.getElementById("result-out").value = countOut;
-};
-
-
-// Увеличение с помощью клавиатуры
+// Увеличение/уменьшение с помощью клавиатуры
 document.addEventListener('keydown', function(event) {
+  let countIn = Number(document.getElementById("result-in").value);
+  let countOut = Number(document.getElementById("result-out").value);
   if (event.code == 'ArrowUp') {
-    var countIn = Number(document.getElementById("result-in").value);
     countIn += 1
-    document.getElementById("result-in").value = countIn;
-    
-  }
-});
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'ArrowDown') {
-    var countOut = Number(document.getElementById("result-out").value);
+  } else if (event.code == 'ArrowDown') {
     countOut += 1;
-    document.getElementById("result-out").value = countOut
-  }
-});
-// Уменьшение с помощью клавиатуры
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'ArrowLeft') {
-    var countIn = Number(document.getElementById("result-in").value);
+  } else if (event.code == 'ArrowLeft') {
     countIn -= 1
-    document.getElementById("result-in").value = countIn;
-    
-  }
-});
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'ArrowRight') {
-    var countOut = Number(document.getElementById("result-out").value);
+  } else if (event.code == 'ArrowRight') {
     countOut -= 1;
-    document.getElementById("result-out").value = countOut
   }
+  document.getElementById("result-in").value = countIn;
+  document.getElementById("result-out").value = countOut;
 });
